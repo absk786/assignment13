59f2d286a49877300d73da0de75bbf0411dffede
absk786 <rehmank11@gmail.com>
2021-08-08 16:55:49 +0100

    updated

--- models/User.js
+++ models/User.js
@@ -1,9 +1,7 @@
-const {Model, DataTypes} =  require('sequlize')
+const {Model, DataTypes, STRING} =  require('sequelize')
 const sequelize = require('sequelize')
 const bcrypt = require('bcypt')
 const exp = require('constants')
-
-
 class User extends Model {
 
 }
@@ -12,16 +10,42 @@ User.init (
     {
 
         id:{
-
+            type: DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement: true
         },
         username:{
-
+            type:DataTypes.STRING,
+            allowNull:false
         },
         email: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+            validate:{
+                isEmail:true
+            }
 
         },
         password:{
-
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                len:[4]
+            }
         }
+},
+{
+    hooks:{
+        
+    },
+    sequelize,
+    timestamps:true,
+    freezeTableName:true,
+    underscored:true,
+    modelName:'User'
 }
 )
+
+module.exports = User;
